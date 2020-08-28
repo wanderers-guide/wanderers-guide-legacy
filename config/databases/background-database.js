9fd71db65d4364afd4c70c2d7c3f415b5d8c98b8
Aaron Cassar <aaronncassar@gmail.com>
2020-08-28 14:00:25 -0700

    Switching databases again

--- config/databases/background-database.js
+++ config/databases/background-database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const keys = require('../keys');
 
-module.exports =  new Sequelize('mysql://doadmin:'+keys.cloudSQL.Password+'@db-mysql-sfo2-63519-do-user-7330312-0.a.db.ondigitalocean.com:25060/'+keys.contentDB.DbName+'?', {
+module.exports =  new Sequelize(keys.contentDB.DbName, keys.cloudSQL.Username, keys.cloudSQL.Password, {
   host: keys.cloudSQL.Host,
   dialect: 'mysql',
   operatorsAliases: '0',
--- config/databases/content-database.js
+++ config/databases/content-database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const keys = require('../keys');
 
-module.exports =  new Sequelize('mysql://doadmin:'+keys.cloudSQL.Password+'@db-mysql-sfo2-63519-do-user-7330312-0.a.db.ondigitalocean.com:25060/'+keys.contentDB.DbName+'?', {
+module.exports =  new Sequelize(keys.contentDB.DbName, keys.cloudSQL.Username, keys.cloudSQL.Password, {
   host: keys.cloudSQL.Host,
   dialect: 'mysql',
   operatorsAliases: '0',
@@ -18,7 +18,7 @@ module.exports =  new Sequelize('mysql://doadmin:'+keys.cloudSQL.Password+'@db-m
 });
 
 /*
-module.exports =  new Sequelize(keys.contentDB.DbName, keys.cloudSQL.Username, keys.cloudSQL.Password, {
+module.exports =  new Sequelize('mysql://doadmin:'+keys.cloudSQL.Password+'@db-mysql-sfo2-63519-do-user-7330312-0.a.db.ondigitalocean.com:25060/'+keys.contentDB.DbName+'?', {
   host: keys.cloudSQL.Host,
   dialect: 'mysql',
   operatorsAliases: '0',
