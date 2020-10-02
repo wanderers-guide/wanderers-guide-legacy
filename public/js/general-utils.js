0c4fbdcc81775eb274add7e73167f915accc459d
Aaron Cassar <aaronncassar@gmail.com>
2020-10-02 05:00:35 -0700

    Added Lost Omens Char Guide

--- public/js/general-utils.js
+++ public/js/general-utils.js
@@ -38,7 +38,7 @@ let g_contentSources = [
   {TextName: 'Bestiary 2', CodeName: 'BEST-2'},
 ];
 
-let g_currentContentSource = 'LOST-CHAR-GUIDE';
+let g_currentContentSource = 'LOST-WORLD-GUIDE';
 
 function getContentSourceTextName(codeName){
   let contentSourceData = g_contentSources.find(contentSourceData => {
