9efe8bfc3a98facc12984e21bb620a6438dcf43a
significantotter <significantotterwrites@gmail.com>
2023-01-29 10:25:01 -0500

    moves legacy character-list code to a legacy file for the sake of example

--- client/vue-src/App.vue
+++ client/vue-src/App.vue
@@ -3,17 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, onBeforeUnmount } from "vue";
 import CharacterList from "./views/dashboard/character-list/character-list.vue";
-import { setup, teardown } from "./legacy-js/character-list.js";
-
-onBeforeMount(() => {
-  setup();
-});
-
-onBeforeUnmount(() => {
-  teardown();
-});
 </script>
 
 <style scoped>
