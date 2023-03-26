d414921cf13486d977918e6fda63263d94be27d8
Heather Nelson <heatherflux@users.noreply.github.com>
2023-03-25 20:50:11 -0500

    add docker start to bash script

--- cdk/lib/bootstrap.sh
+++ cdk/lib/bootstrap.sh
@@ -11,6 +11,8 @@ sudo amazon-linux-extras install docker
 sudo usermod -a -G docker ec2-user
 # Download and install
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo service docker start
+
 # Fix permissions
 sudo chmod +x /usr/local/bin/docker-compose
 sudo aws configure --region us-east-2 --output json --profile default
